20ec952518ba06ba95bb5985b6e9f977787153b2
Fizal Sarif <fizal.sarif@gmail.com>
2016-06-16 02:40:56 +0000

    changed random buzz words

--- js/app.js
+++ js/app.js
@@ -1,3 +1,32 @@
 $(function(){
+
   console.log('loaded');
+
+  //declare some stuff
+  let languages = ['HTML', 'CSS', 'JavaScript', 'jQuery', 'Python', 'Node.js', 'React.js', 'GIT', 'API'];
+  
+
+  //randomly spawn these then fade them out
+  function drawLanguage(){
+  	let language = Math.floor(Math.random()*languages.length);
+
+  	//make the h2 element
+  	let h2 = $('<h2>');
+  	h2.text(languages[language]);
+  	h2.addClass('language');
+
+  	// determine the random x and y
+  	var posX = Math.floor(Math.random() * ($('.first').width() - 50));
+    var posY = Math.floor(Math.random() * ($('.first').height() - 50));
+    h2.css('left', posX + 'px');
+    h2.css('top', posY + 'px');
+
+    // append it
+    $(h2).appendTo('.first').fadeIn(500).delay(5000).fadeOut(500, function() {
+      $(this).remove();
+    });
+
+  }
+
+  setInterval(drawLanguage, 1000);
 });
